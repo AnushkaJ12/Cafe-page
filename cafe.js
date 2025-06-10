d2a0061a5ca8729853fd489c33c2d3a6e643adeb
AnushkaJ12 <147900511+AnushkaJ12@users.noreply.github.com>
2025-06-10 12:40:41 +0530

    Update cafe.js

---
+++ cafe.js
@@ -0,0 +1,23 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll('.fade-in');
+
+  const appearOnScroll = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1
+    }
+  );
+
+  faders.forEach(fadeEl => {
+    appearOnScroll.observe(fadeEl);
+  });
+});
